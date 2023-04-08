7d7b30ca532f1c94a85b32350dc595665c0e5a96
themimd6 <129931360+themimd6@users.noreply.github.com>
2023-04-08 07:09:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = "OXhzZjFCRU4=" //session Id
+global.session = "aHhDTG1RMVA=" //session Id
 global.ownernomer = "94786802371" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94786802371'] //ur premium number
